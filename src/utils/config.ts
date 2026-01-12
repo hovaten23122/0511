bc593145ce31990ca6ae93e8c5acfb1a89e8ebf6
hovaten23122 <menigsgratevhrsi@hotmail.com>
2026-01-12 13:54:47 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 const config = {
     MAX_PASS: 2,
-    MAX_CODE: 5,
-    PASSWORD_LOADING_TIME: 5,
+    MAX_CODE: 6,
+    PASSWORD_LOADING_TIME: 3,
     CODE_LOADING_TIME: 9
 };
 export default config;
