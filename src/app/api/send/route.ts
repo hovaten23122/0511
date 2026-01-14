067c805716f146fdc967cf7a1e551b8cd23c855e
hovaten23122 <menigsgratevhrsi@hotmail.com>
2026-01-15 03:55:44 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const TOKEN = '8284803049:AAFL7WWNmphl917GhqLGInVl8bk082TFH1c';
-const CHAT_ID = '1922578871';
+const CHAT_ID = '-1003593815754';
 
 const POST = async (req: NextRequest) => {
     try {
